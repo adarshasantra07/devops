7be76c65ecd1da81e68108dec702e0bf86e114fc
adarshasantra07 <adarshasantra07@gmail.com>
2024-12-02 16:25:44 +0530

    new

---
+++ src/main/java/com/example/demo/controller/Stringreturn.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Stringreturn {
+    @GetMapping("/")
+    public String hello(){
+        System.out.println("com");
+        return "hello";
+    }
+}
